eefe1c7fa5b5e41a590babfd4863f66613f6245c
soumya-135 <soumyamundharikar@gmail.com>
2026-09-23 20:29:38 +0530

    new file

---
+++ src/LinearSearch/SearchInRange.java
@@ -0,0 +1,4 @@
+package LinearSearch;
+
+public class SearchInRange {
+}
--- src/LinearSearch/SearchInStringByForEach.java
+++ src/LinearSearch/SearchInStringByForEach.java
@@ -7,7 +7,15 @@ public class SearchInStringByForEach {public static void main(String[] args) {
     String name = "Soumya Mundharikar";
     char target = 'u';
 
-    System.out.println(Arrays.toString(name.toCharArray()));
+    //name.toCharArray bass String ko Array me convert krega and Yaha hum usko
+    //print kr rhe hai;
+    System.out.println(Arrays.toString(name.toCharArray()));//this is for Str=array conversion and print
+    //we dont need function for this just make a string and sout lagao
+    //automatically it will print the string in Array format;
+
+    //Yha I'm calling search 2 function so that
+    // our search 2 can print T/F for the target
+    System.out.println(search2(name,target));//only to print target result;
 }
     static boolean search2(String str,char target){
         if (str.length() == 0){
